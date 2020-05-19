2d074fe61196ed5d730c67ebc0498d0dc2f8096e
Farzad Daud <daud.farzad1@gmail.com>
2020-05-19 12:48:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,8 +6,8 @@ def introduction (Farzad)
   puts "Hi, my name is #{Farzad}."
 end
 
-def introduction_with_language (name,language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction_with_language (Farzad,Ruby)
+  puts "Hi, my name is #{Farzad} and I am learning to program in #{Ruby}."
 end
 
 
